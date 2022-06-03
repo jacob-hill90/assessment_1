debc04caab46b6fccd99da922e977b5ce32ee942
Jacob Hill <hilljake93@gmail.com>
2022-06-03 16:47:16 -0500

    second commit - very frustrated with formatting

--- optimal_change.py
+++ optimal_change.py
@@ -2,16 +2,16 @@ def optimal_change(item_cost, amount_paid):
     
     #When exact change is given
     if(item_cost == amount_paid):
-        print("Wow, You've payed with exact change and made this assessment way easier for me!")
+        return "Wow, You've payed with exact change and made this assessment way easier for me!"
     
     #When item cost isn't met by amount paid
     elif(item_cost > amount_paid):
-        print("Uh oh, Looks like you haven't paid enough!")
+        return "Uh oh, Looks like you haven't paid enough!"
     
     #When amount paid is greater than item cost and we need to give change
     elif item_cost < amount_paid:
         #Create str which should be added to once we know how much change to give
-        answer_string = f'The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is'
+        answer_string = f'The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is '
         array_of_keys = []
         #Convert arguments to pennies so it's easier to work with
         amount_paid_in_pennies = amount_paid * 100
@@ -30,6 +30,7 @@ def optimal_change(item_cost, amount_paid):
             "nickel": 5,
             "penny": 1
         }
+    
         #Use dictionary to subtract from total change, once largest denomination left is found
         for key in dict:
             while change_in_pennies >= dict[key]:
@@ -37,25 +38,38 @@ def optimal_change(item_cost, amount_paid):
                 change_in_pennies -= dict[key]
         #Create another dictionary with denominations and occurances 
         dict_of_change_values = {x:array_of_keys.count(x) for x in array_of_keys}
-        
-        #create string to be added to, depending on number of occurances              
-        added_string = ''
+         
+        print(dict_of_change_values)
 
-        for i in dict_of_change_values:
-            #plural and singular if statements
-            if i == '100' or i == '50' or i == '20' or i == '10' or i == '5' or i == '1':
-                if dict_of_change_values[i] == 1:
-                    added_string += f'{dict_of_change_values[i]} ${i} dollar bill, '
-                elif dict_of_change_values[i] > 1:
-                   added_string += f'{dict_of_change_values[i]} ${i} dollar bills, ' 
-            elif i == 'quarter' or i == 'dime' or i == 'nickel' or 'penny':
-                if dict_of_change_values[i] == 1:
-                    added_string += f'{dict_of_change_values[i]} {i} '
-                elif dict_of_change_values[i] > 1 and (i == 'penny'):
-                    added_string += f'{dict_of_change_values[i]} pennies'
-                elif dict_of_change_values[i] > 1 and (i == 'quarter' or i == 'dime' or i == 'nickel'):
-                    added_string += f'{dict_of_change_values[i]} {i}s '
+optimal_change(20.31,25)
 
-        print(answer_string,added_string+".")
 
-optimal_change(31.54, 50)
+#Very very frustrating, don't have the tools/knowledge to format this fucking sentance correctly. 
+
+       # if i == '100' or i == '50' or i == '20' or i == '10' or i == '5' or i == '1':
+    #     if dict_of_change_values[i] == 1:
+    #         added_string += f'{dict_of_change_values[i]} ${i} dollar bill'
+    #     elif dict_of_change_values[i] > 1:
+    #         added_string += f'{dict_of_change_values[i]} ${i} dollar bills' 
+    # elif i == 'quarter' or i == 'dime' or i == 'nickel' or 'penny':
+    #     if dict_of_change_values[i] == 1:
+    #         added_string += f'{dict_of_change_values[i]} {i}'
+    #     elif dict_of_change_values[i] > 1 and (i == 'penny'):
+    #         added_string += f'and {dict_of_change_values[i]} pennies'
+    #     elif dict_of_change_values[i] > 1 and (i == 'quarter' or i == 'dime' or i == 'nickel'):
+    #         added_string += f'{dict_of_change_values[i]} {i}s'
+    # return answer_string+added_string+"."  
+
+    # if i == '100' or i == '50' or i == '20' or i == '10' or i == '5' or i == '1':
+    #     if dict_of_change_values[i] == 1:
+    #         added_string += f'{dict_of_change_values[i]} ${i} dollar bill'
+    #     elif dict_of_change_values[i] > 1:
+    #         added_string += f'{dict_of_change_values[i]} ${i} dollar bills' 
+    # elif i == 'quarter' or i == 'dime' or i == 'nickel' or 'penny':
+    #     if dict_of_change_values[i] == 1:
+    #         added_string += f'{dict_of_change_values[i]} {i}'
+    #     elif dict_of_change_values[i] > 1 and (i == 'penny'):
+    #         added_string += f'and {dict_of_change_values[i]} pennies'
+    #     elif dict_of_change_values[i] > 1 and (i == 'quarter' or i == 'dime' or i == 'nickel'):
+    #         added_string += f'{dict_of_change_values[i]} {i}s'
+    # return answer_string+added_string+"."
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,17 +1,9 @@
 from optimal_change import optimal_change
-
 print("1:", optimal_change(62.13, 100) == "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
-
 print("2:", optimal_change(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
-
-print("3:", optimal_change(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
-
-print("4:", optimal_change(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
-
-print("5:", optimal_change(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
-
-print("6:", optimal_change(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
-
-print("7:", optimal_change(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
-
-print("8:", optimal_change(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+print("3:", optimal_change(45.23, 60) == "The optimal change for an item that costs $45.23 with an amount paid of $60 is 1 $10 bill, 4 $1 bills, 3 quarters, and 2 pennies.")
+print("4:", optimal_change(20.31, 25) == "The optimal change for an item that costs $20.31 with an amount paid of $25 is 4 $1 bills, 2 quarters, 1 dime, 1 nickel, and 4 pennies.")
+print("5:", optimal_change(99.50, 100) == "The optimal change for an item that costs $99.5 with an amount paid of $100 is 2 quarters.")
+print("6:", optimal_change(10, 20) == "The optimal change for an item that costs $10 with an amount paid of $20 is 1 $10 dollar bill.")
+print("7:", optimal_change(40, 30) == "Uh oh, Looks like you haven't paid enough!")
+print("8:", optimal_change(25, 25) == "Wow, You've payed with exact change and made this assessment way easier for me!")
